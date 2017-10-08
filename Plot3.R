aafab514661cfd100fa71e47403df82159f27829
AlexPouletsos <30276007+AlexPouletsos@users.noreply.github.com>
2017-10-08 00:36:57 -0400

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -14,7 +14,7 @@ hpcSub <- cbind(DateTime,hpcSub)
 
 #Making the plot
 
-png(filename = "plot3.png", height=480, width=480)
+png(filename = "plot3.png", height=480, width=480) #saves png file to working directory
 
 with(hpcSub, plot(hpcSub$DateTime, as.numeric(as.character(Sub_metering_1)),
         type="l", xlab="", ylab="Energy sub metering", col="black"))
